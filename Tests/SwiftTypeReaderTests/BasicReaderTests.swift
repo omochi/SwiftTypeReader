aede47d2c0f9545b924dc2ab85d73688c26252bf
omochimetaru <omochi.metaru@gmail.com>
2022-11-22 08:26:59 +0900

    update readme test

--- Tests/SwiftTypeReaderTests/BasicReaderTests.swift
+++ Tests/SwiftTypeReaderTests/BasicReaderTests.swift
@@ -2,6 +2,38 @@ import XCTest
 import SwiftTypeReader
 
 final class BasicReaderTests: ReaderTestCaseBase {
+    func testReadmeExample() throws {
+        try withExtendedLifetime(Context()) { (context) in
+            let module = context.getOrCreateModule(name: "main")
+            let reader = Reader(context: context, module: module)
+
+            let source = try reader.read(
+                source: """
+struct S {
+    var a: Int?
+}
+""",
+                file: URL(fileURLWithPath: "S.swift")
+            )
+            _ = source
+
+            let s = try XCTUnwrap(module.find(name: "S") as? StructDecl)
+            XCTAssertEqual(s.name, "S")
+
+            XCTAssertEqual(s.storedProperties.count, 1)
+            let a = try XCTUnwrap(s.find(name: "a") as? VarDecl)
+            XCTAssertIdentical(a, s.storedProperties[safe: 0])
+            XCTAssertEqual(a.name, "a")
+
+            let aType = try XCTUnwrap(a.interfaceType as? EnumType)
+            XCTAssertEqual(aType.name, "Optional")
+            XCTAssertEqual(aType.genericArgs.count, 1)
+
+            let aWrappedType = try XCTUnwrap(aType.genericArgs[safe: 0] as? StructType)
+            XCTAssertEqual(aWrappedType.name, "Int")
+        }
+    }
+
     func testSimpleStruct() throws {
         let module = try read("""
 struct S {
